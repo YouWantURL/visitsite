3aaeea11e981ae8e90d3076546b9b715765d6c15
YouWantURL <mecel.andrey@gmail.com>
2025-02-11 22:23:23 +0300

    .//

--- media.js
+++ media.js
@@ -26,7 +26,7 @@ wavesurfer.init({
 
 
 //загружаем нужную песню
-wavesurfer.load("sound.mp3");
+wavesurfer.load("sound1.mp3");
 
 //play and pause a player
 playPause.addEventListener("click", function (e) {
